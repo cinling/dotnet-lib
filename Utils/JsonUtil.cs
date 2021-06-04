0ca0607630a2a99bd1b29e02a04c9f312fa75766
Cinling <1105412681@qq.com>
2021-06-04 11:42:59 +0800

    添加测试代码

---
+++ Utils/JsonUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace Cinling.Lib.Utils {
+    
+    /// <summary>
+    /// JSON encode and decode tools
+    /// JSON 序列化、反序列化工具
+    /// </summary>
+    public class JsonUtil {
+        /// <summary>
+        /// JSON encode
+        /// JSON 序列化
+        /// </summary>
+        /// <returns></returns>
+        public static string encode(object value) {
+            return JsonSerializer.Serialize(value);
+        }       
+    }
+}
